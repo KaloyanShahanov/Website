0a78661e5138054256e41a4cd0cc07fb709eb7f4
KaloqnS <arhangel9808@gmail.com>
2025-05-16 17:49:02 +0300

    changed app.py

--- app.py
+++ app.py
@@ -1,11 +1,7 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from Flask on Render!"
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=10000)
-
+    return render_template("index.html")
